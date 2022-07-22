744fae72f57621f0c30ea90df57666cdb24fd24b
Anthony Maras <marasantonis@gmail.com>
2022-07-22 15:06:43 +0300

    refactor to class

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import warningIcon from './assets/images/icon-error.svg';
-import arrowIcon from './assets/images/icon-arrow.svg';
 import heroImgMobile from './assets/images/hero-mobile.jpg';
 import heroImgDesktop from './assets/images/hero-desktop.jpg';
+import logo from './assets/images/logo.svg';
+import { Email } from './components/forms';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div>Title</div>
-      <picture>
-        <source media="(min-width:1440px)" srcSet={heroImgDesktop} />
-        <img src={heroImgMobile} alt="woman photograph" />
-      </picture>
-      <div>
-        <div>
-          We're<span> coming soon</span>
-        </div>
-        <p>
-          Hello fellow shoppers! We're currently building our new fashion store.
-          Add your email below to stay up-to-date with announcements and our
-          launch deals
-        </p>
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <section className="logo-wrapper">
+          <img src={logo} alt="company logo"></img>
+        </section>
+        <picture>
+          <source media="(min-width:1440px)" srcSet={heroImgDesktop} />
+          <img src={heroImgMobile} alt="woman photograph" />
+        </picture>
+        <section className="statement-wrapper">
+          <div>
+            We're<span> coming soon</span>
+          </div>
+          <div>
+            Hello fellow shoppers! We're currently building our new fashion
+            store. Add your email below to stay up-to-date with announcements
+            and our launch deals
+          </div>
+        </section>
+        <Email />
       </div>
-      <form>
-        <div>
-          <label htmlFor="email">email</label>
-          <input type="email"></input>
-          <img src={warningIcon} alt="warning icon"></img>
-          <p>Please provide a valid email</p>
-        </div>
-        <button type="submit">
-          <img src={arrowIcon} alt="arrow icon "></img>
-        </button>
-      </form>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
